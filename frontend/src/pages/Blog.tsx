181dfa6cd545416412df5068a09ac1a132f92d02
madhav-relish <madhavsingh.relish@gmail.com>
2024-07-04 10:36:04 +0530

    Individual Blogs + Other Changes
    - Integrated /blg/:id page
    - When we click on the read more button on the blog card the redirection
      has been added
    - Integrated the title and content html in the ui for retreived blogs

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,9 +1,48 @@
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { BACKEND_URL } from "../config";
+import { useEffect, useState } from "react";
 
-
-const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
+interface BlogData {
+  content: string;
+  title: string;
 }
+const Blog = () => {
+  const { id } = useParams<{ id: string }>();
+  const [blog, setBlog] = useState<BlogData | null>(null);
 
-export default Blog
+  const fetchBlog = async () => {
+    try {
+      const token = localStorage.getItem("accessToken");
+      const response = await axios(BACKEND_URL + `/api/v1/blog/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(response.data);
+      setBlog(response.data);
+    } catch (error) {
+      console.log("Error occured while fetching the blog::", error);
+    }
+  };
+
+  const ele = document?.getElementById("content-block") || null;
+  if (ele) {
+    ele.innerHTML = blog?.content || "";
+  }
+
+  useEffect(() => {
+    fetchBlog();
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-8">
+      {/* Title */}
+      <h1 className="text-3xl font-semibold">{blog?.title}</h1>
+      {/* Content */}
+      <div id="content-block"></div>
+    </div>
+  );
+};
+
+export default Blog;
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,41 +1,55 @@
-import axios from "axios"
-import BlogCard from "../components/BlogCard"
-import { BACKEND_URL } from "../config"
-import { useEffect } from "react"
-
+import axios from "axios";
+import BlogCard from "../components/BlogCard";
+import { BACKEND_URL } from "../config";
+import { useEffect, useState } from "react";
+import { Loader } from "@mantine/core";
 
 const Blogs = () => {
-  const fetchAllBlogs = async () =>{
-    try{
-      const token = localStorage.getItem('accessToken')
+  const [allBlogs, setAllBlogs] = useState<[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchAllBlogs = async () => {
+    try {
+      const token = localStorage.getItem("accessToken");
       const response = await axios.get(BACKEND_URL + `/api/v1/blog/bulk`, {
         headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-      console.log(response.data)
-    }catch(error){
-      console.error("Error while fetching all blogs", error)
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      // console.log(response.data);
+      setAllBlogs(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error("Error while fetching all blogs", error);
     }
-  }
+  };
 
-  useEffect(()=>{
-    fetchAllBlogs()
-  },[])
+  useEffect(() => {
+    fetchAllBlogs();
+  }, []);
   return (
     <div className="p-4">
-      <div className="flex gap-4 flex-wrap">
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      </div>
+      {loading ? (
+        <div className="h-screen w-full flex justify-center items-center">
+          {" "}
+          <Loader color="blue" />{" "}
+        </div>
+      ) : (
+        <div className="flex gap-4 flex-wrap">
+          {allBlogs?.map((blog, idx) => (
+            <BlogCard
+              key={idx}
+              title={blog["title"]}
+              content={blog["content"]}
+              date=""
+              blogId={blog["id"]}
+            />
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Blogs
+export default Blogs;
