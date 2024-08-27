e4d29a37fcccc209278f81f5b8cc7e4b34c21dcf
madhav-relish <madhavsingh.relish@gmail.com>
2024-08-27 12:24:10 +0530

    Changed the common package

--- common/src/index.ts
+++ common/src/index.ts
@@ -13,12 +13,14 @@ export const signinInput = z.object({
 
 export const createPost = z.object({
     title: z.string(),
-    content: z.string()
+    content: z.string(),
+    published: z.boolean().optional()
 })
 
 export const updatePost = z.object({
     title: z.string().optional(),
-    content: z.string().optional()
+    content: z.string().optional(),
+    published: z.boolean().optional()
 })
 
 export type SignupInput = z.infer<typeof signupInput>
